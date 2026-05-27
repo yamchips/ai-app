241633fc99e202389b31b673a63e83f162020ba7
yamchips <wflunch@gmail.com>
2026-05-27 17:04:15 +0100

    Manage OpenAI API key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import type {Request, Response} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world')
+    res.send("Hello world!")
 })
 
 app.listen(port, () => {
