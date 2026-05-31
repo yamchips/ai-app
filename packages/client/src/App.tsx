0038fd6b9d79c4fb3b8526dec4a4dc918027ecae
yamchips <wflunch@gmail.com>
2026-05-31 11:01:13 +0100

    Build a chatbot dialog box

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Button } from '@/components/ui/button';
+import ChatBot from './components/ChatBot';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
   return (
     <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button variant="outline">Hello World</Button>
+      <ChatBot />
     </div>
   );
 }
